2b2f40226ac9a92ecf9d6187217563a3be5f30f0
mikelhpdatkel <mikelhpdatke@gmail.com>
2018-10-18 09:44:05 +0700

    firstss

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import openSocket from 'socket.io-client';
+import React, { Component } from 'react';
+const  socket = openSocket('http://localhost:8000');
+function subscribeToTimer(cb) {
+  socket.on('timer', timestamp => cb(null, timestamp));
+  socket.emit('subscribeToTimer', 1000);
+}
+export { subscribeToTimer };
